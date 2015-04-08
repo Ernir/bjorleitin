d2dbed58e0ce6009551bf3e14b0a7b2f66098eb2
Ernir <e.ernir@gmail.com>
2015-04-08 17:27:49 +0000

    Very basic beer list added.

--- beer_search/views.py
+++ beer_search/views.py
@@ -1,5 +1,7 @@
+from beer_search.models import Beer
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html", {})
+    all_beers = Beer.objects.all()
+    return render(request, "index.html", {"beers": all_beers})
