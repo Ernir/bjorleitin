0ff891914baad7e254f9f001659a3d5a90de5e56
Ernir <e.ernir@gmail.com>
2015-04-08 14:08:37 +0000

    Switched to WhiteNoise.

--- BeerSearch/settings.py
+++ BeerSearch/settings.py
@@ -113,3 +113,7 @@ ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
