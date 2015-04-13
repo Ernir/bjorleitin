11e463bcbc8b21ff834eea5e0db83bfa30bc8321
Ernir <e.ernir@gmail.com>
2015-04-13 00:07:55 +0000

    Overview debug option added

--- beer_search/views.py
+++ beer_search/views.py
@@ -1,11 +1,14 @@
 from beer_search.models import Beer
 from django.shortcuts import render
+from BeerSearch.settings import DEBUG
 
 
 def index(request):
     all_beers = Beer.objects.all()
     return render(request, "index.html", {"beers": all_beers})
 
+
 def overview(request):
     all_beers = Beer.objects.all()
-    return render(request, "overview.html", {"beers": all_beers})
+
+    return render(request, "overview.html", {"beers": all_beers, "debug": DEBUG})
