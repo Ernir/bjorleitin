f7f76a2a70f8f1a117f125c5056849e4532bd5cf
Ernir <e.ernir@gmail.com>
2015-05-29 15:30:19 +0000

    Store form temporarily commented out.

--- beer_search/forms.py
+++ beer_search/forms.py
@@ -217,13 +217,13 @@ class SearchForm(forms.Form):
                 css_class="row"
             ),
             Div(
-                Div(
-                    Div(
-                        Field("stores"),
-                        css_class="checkbox"
-                    ),
-                    css_class="col-md-6"
-                ),
+                # Div(
+                #     Div(
+                #         Field("stores"),
+                #         css_class="checkbox"
+                #     ),
+                #     css_class="col-md-6"
+                # ),
                 Div(
                     Div(
                         Field("styles"),
