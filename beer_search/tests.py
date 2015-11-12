f403f74fa51509241487b1733f47e761fbe28bbf
Ernir <e.ernir@gmail.com>
2015-11-12 12:53:48 +0000

    Add basic 200-checks to the views emitting HTML
    The app is getting scary to modify. This represents an effort to at
    least make it easier to tell when things go wrong.

--- beer_search/tests.py
+++ beer_search/tests.py
@@ -1,3 +1,41 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class BeerViewsTestCase(TestCase):
+
+    """
+    Unit tests for views returning HTML web pages.
+    """
+
+    def test_index(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_table_loader(self):
+        response = self.client.get(reverse("index_table"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_overview(self):
+        response = self.client.get(reverse("overview"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_exciting(self):
+        response = self.client.get(reverse("exciting"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_gift_boxes(self):
+        response = self.client.get(reverse("gift_boxes"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_statistics(self):
+        response = self.client.get(reverse("statistics"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_doc(self):
+        response = self.client.get(reverse("api-doc"))
+        self.assertEqual(response.status_code, 200)
