6c472559038d9ab940b6d2032ee2362e6d86b0d1
Ernir <e.ernir@gmail.com>
2016-07-23 23:01:40 +0000

    Remove all (ok, most) traces of version 1!

--- BeerSearch/wsgi.py
+++ BeerSearch/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BeerSearch.settings")
 
-from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
