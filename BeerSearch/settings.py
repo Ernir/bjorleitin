b8b3cd8131d894522fa2f77b03d4cf1e15b0cffd
Ernir <e.ernir@gmail.com>
2016-07-23 23:01:40 +0000

    REMOVE ALL (ok, most) TRACES OF VERSION 1git add .!

--- BeerSearch/settings.py
+++ BeerSearch/settings.py
@@ -36,12 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     "debug_toolbar",
     "beer_search_v2",
-    "storages",
-    "compressor",
     "rest_framework"
 )
-
-CRISPY_TEMPLATE_PACK = "bootstrap3"
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -84,21 +80,15 @@ TEMPLATES = [
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 )
 
 WSGI_APPLICATION = 'BeerSearch.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-# Parse database configuration from $DATABASE_URL
-DATABASES = {"default": dj_database_url.config("BEER_DB_URL")}
+DATABASES = {"default": {}}
 # Enable Connection Pooling
 DATABASES["default"]["ENGINE"] = "django_postgrespool"
-# Stuff from Heroku quickstart:
-
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config("BEER_DB_URL")
 
@@ -106,13 +96,9 @@ DATABASES['default'] = dj_database_url.config("BEER_DB_URL")
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'is'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 if not DEBUG:
@@ -136,53 +122,16 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Twitter setup
-
 TWITTER_CONSUMER_KEY = os.environ.get("BEER_TWITTER_CONSUMER_KEY")
 TWITTER_CONSUMER_SECRET = os.environ.get("BEER_TWITTER_CONSUMER_SECRET")
 TWITTER_ACCESS_TOKEN = os.environ.get("BEER_TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("BEER_TWITTER_ACCESS_TOKEN_SECRET")
-
-# AWS setup
-AWS_ACCESS_KEY_ID = os.environ.get("BEER_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("BEER_AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "bjorleit"
-AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = False
-# AWS_IS_GZIPPED = True
-
-AWS_HEADERS = {
-    "Cache-Control": "max-age=86400",  # 24 hours
-}
-
-# Media file configuration
-
-DEFAULT_FILE_STORAGE = 'BeerSearch.s3utils.MediaRootS3BotoStorage'
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 
 # Static file configuration
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-STATIC_URL = '//%s.s3.amazonaws.com/compressor/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Compressor configuration
-COMPRESS_ENABLED = bool(int(os.environ.get("COMPRESS_ENABLED")))
-if COMPRESS_ENABLED:
-    STATICFILES_STORAGE = 'BeerSearch.s3utils.CompressorS3BotoStorage'
-    COMPRESS_OFFLINE = True
-    COMPRESS_URL = STATIC_URL
-    COMPRESS_STORAGE = STATICFILES_STORAGE
-    COMPRESS_ROOT = STATIC_ROOT
-    COMPRESS_CSS_FILTERS = [
-        'compressor.filters.css_default.CssAbsoluteFilter',
-        'compressor.filters.cssmin.CSSMinFilter',
-        'compressor.filters.jsmin.JSMinFilter'
-    ]
-else:
-    STATICFILES_STORAGE = 'BeerSearch.s3utils.StaticRootS3BotoStorage'
-
-if not COMPRESS_ENABLED:
-    STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
