be5f022cbf8b30bd46ca3d39e3137a3d1d77a4f2
Ernir <e.ernir@gmail.com>
2016-08-04 11:26:51 +0000

    Add optional arguments for single item updates and verbosity

--- beer_search_v2/management/commands/update_stock_v2.py
+++ beer_search_v2/management/commands/update_stock_v2.py
@@ -2,6 +2,7 @@ import requests
 from beer_search_v2.utils import renew_cache
 from bs4 import BeautifulSoup
 from beer_search_v2.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 
 
@@ -34,19 +35,43 @@ class Command(BaseCommand):
             info.append({"store": store.string, "stock": int(store.next_sibling.string)})
         return info
 
+    def process_product(self, product, verbose=True):
+        stock_info = self.get_product_data(product.atvr_id)
+        product.available_in_atvr = not not stock_info  # Forcing it to a boolean
+        if verbose:
+            if product.available_in_atvr:
+                availability = "available"
+            else:
+                availability = "unavailable"
+            print("Updating {}, {}".format(str(product), availability))
+        product.atvr_stock = stock_info
+        product.save()
+        product.product_type.update_availability(verbose=verbose)
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+                "--verbose",
+                dest="verbose",
+                help="specify how much the script should write",
+                default=True,
+                action="store_true"
+        )
+        parser.add_argument(
+                "--item",
+                dest="item",
+                help="specify a particular product to update"
+        )
+
     def handle(self, *args, **options):
-        verbose = True
-        products = Product.objects.all()
-        for product in products:
-            stock_info = self.get_product_data(product.atvr_id)
-            product.available_in_atvr = not not stock_info  # Forcing it to a boolean
-            if verbose:
-                if product.available_in_atvr:
-                    availability = "available"
-                else:
-                    availability = "unavailable"
-                print("Updating {}, {}".format(str(product), availability))
-            product.atvr_stock = stock_info
-            product.save()
-            product.product_type.update_availability(verbose=verbose)
-        renew_cache()
+        verbose = not not options["verbose"]
+        if options["item"]:  # If an item is supplied, we only process that one
+            try:
+                product = Product.objects.get(atvr_id=options["item"])
+                self.process_product(product, verbose)
+            except ObjectDoesNotExist:
+                print("Nothing found for {}.".format(options["item"]))
+        else:  # Otherwise, we process them all
+            products = Product.objects.filter(atvr_id__isnull=False).all()
+            for product in products:
+                self.process_product(product, verbose)
+            renew_cache()
